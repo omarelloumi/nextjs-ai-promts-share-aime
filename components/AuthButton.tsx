102829fb99472cd6686bf56296cfec8594da34e8
omarelloumi <48672001+omarelloumi@users.noreply.github.com>
2023-05-17 13:43:08 +0100

    Update AuthButton.tsx

--- components/AuthButton.tsx
+++ components/AuthButton.tsx
@@ -1,19 +1,10 @@
 "use client"
 
-import { signIn, signOut, useSession } from "next-auth/react"
+import { signIn } from "next-auth/react"
 
 const SigninButton = () => {
-  const { data: session } = useSession()
-
-  if (session && session.user) {
-    return (
-    <button onClick={() =>signOut()} className="bg-red-600 hover:bg-red-800 transition text-white rounded-2xl py-2 px-4">
-          Sign Out
-    </button>
-    );
-  }
   return (
-    <button onClick={() => signIn()} className="bg-blue-600 hover:bg-blue-800 transition text-white rounded-2xl py-2 px-4">
+    <button onClick={() => signIn()} className="bg-purple-600 hover:bg-purple-800 transition text-white rounded-2xl py-2 px-4">
       Sign In
     </button>
   );
