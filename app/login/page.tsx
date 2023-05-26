ae3043df50195c2e3c3dd25681dc0b5590f24b51
omarelloumi <48672001+omarelloumi@users.noreply.github.com>
2023-05-26 07:36:50 +0100

    create login route

---
+++ app/login/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import Login from '@/components/forms/Login'
+
+type Props = {}
+
+const page = (props: Props) => {
+  return (
+    <Login/>
+  )
+}
+
+export default page
--- components/forms/Login.tsx
+++ components/forms/Login.tsx
@@ -2,6 +2,7 @@ import { useRef, useState } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
 import axios from "axios";
 import { signIn } from "next-auth/react";
+import Link from "next/link";
 import Logo from "../Logo";
 
 interface LoginUser {
@@ -10,14 +11,13 @@ interface LoginUser {
 }
 
 type Props = {
-  setAuthModal: Function;
+  setAuthModal?: Function;
 };
 
 const Login = (props: Props) => {
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors, isValid },
   } = useForm<LoginUser>({
     mode: "onChange",
@@ -39,16 +39,16 @@ const Login = (props: Props) => {
           username: data.email,
           password: data.password,
         });
-        props.setAuthModal("");
+        props.setAuthModal && props.setAuthModal("");
       }
     } catch (err: any) {
       setSubmitError(err.response.data.error);
     }
   };
 
-  const handleClose = (e: any) => {
+  const handleClose = (e: any): void => {
     if (backdropRef.current === e.target) {
-      props.setAuthModal("");
+      props.setAuthModal && props.setAuthModal("");
     }
   };
 
@@ -61,25 +61,29 @@ const Login = (props: Props) => {
       <div className="relative max-h-full w-full max-w-md">
         {/* Modal content */}
         <div className="relative rounded-lg bg-white shadow dark:bg-gray-700">
-          <button
-            onClick={() => props.setAuthModal("")}
-            className="absolute right-2.5 top-3 ml-auto inline-flex items-center rounded-lg bg-transparent p-1.5 text-sm text-gray-400 hover:bg-gray-200 hover:text-gray-900 dark:hover:bg-gray-800 dark:hover:text-white"
-          >
-            <svg
-              aria-hidden="true"
-              className="h-5 w-5"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
+          {props.setAuthModal && (
+            <button
+              onClick={() => {
+                props.setAuthModal && props.setAuthModal("");
+              }}
+              className="absolute right-2.5 top-3 ml-auto inline-flex items-center rounded-lg bg-transparent p-1.5 text-sm text-gray-400 hover:bg-gray-200 hover:text-gray-900 dark:hover:bg-gray-800 dark:hover:text-white"
             >
-              <path
-                // fill-rule="evenodd"
-                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                // clip-rule="evenodd"
-              ></path>
-            </svg>
-            <span className="sr-only">Close modal</span>
-          </button>
+              <svg
+                aria-hidden="true"
+                className="h-5 w-5"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  // fill-rule="evenodd"
+                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                  // clip-rule="evenodd"
+                ></path>
+              </svg>
+              <span className="sr-only">Close modal</span>
+            </button>
+          )}
           <div className="px-6 py-6 lg:px-8">
             <Logo />
             <h3 className="mb-4 mt-2 flex text-xl font-medium text-gray-900 dark:text-white">
@@ -160,12 +164,24 @@ const Login = (props: Props) => {
               </button>
               <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
                 You do not have an account?{" "}
-                <button
-                  onClick={() => props.setAuthModal("register")}
-                  className="text-purple-700 hover:underline dark:text-purple-500"
-                >
-                  Create one here
-                </button>
+                {props.setAuthModal ? (
+                  <button
+                    onClick={() => {
+                      props.setAuthModal!("register");
+                    }}
+                    className="text-purple-700 hover:underline dark:text-purple-500"
+                  >
+                    Create one here
+                  </button>
+                ) : (
+                  <Link
+                    href="/register"
+                    className="text-purple-700 hover:underline dark:text-purple-500"
+                  >
+                    {" "}
+                    Create one here
+                  </Link>
+                )}
               </div>
             </form>
           </div>
