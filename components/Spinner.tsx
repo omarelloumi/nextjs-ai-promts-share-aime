2ce4c7ef5542845f13c495cb9a9756746be7882c
omarelloumi <48672001+omarelloumi@users.noreply.github.com>
2023-05-30 19:02:21 +0100

    Update Spinner.tsx

--- components/Spinner.tsx
+++ components/Spinner.tsx
@@ -1,8 +1,4 @@
-import React from 'react'
-
-type Props = {}
-
-const Spinner = (props: Props) => {
+const Spinner = () => {
   return (
     <div role="status">
     <svg aria-hidden="true" className="inline w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-purple-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
